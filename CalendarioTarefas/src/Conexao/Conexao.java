e9fd4481fb4c3832202f4bb6b24d2277dc5c8e81
GAOliveira <GAOliveira@GSA07.gsa.go>
2016-02-11 14:51:46 -0200

    Criação da classe de conexão ao banco.

---
+++ CalendarioTarefas/src/Conexao/Conexao.java
@@ -0,0 +1,60 @@
+package Conexao;
+
+
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.Statement;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author GAOliveira
+ */
+public class Conexao {
+    
+    public Statement comando;
+    public Connection connection;
+    
+public Connection getConexao() {  
+    
+    
+        try {  
+            // Carregando o JDBC Driver padrão  
+            String driverName = "com.mysql.jdbc.Driver";                          
+            Class.forName(driverName);  
+            // Configurando a nossa conexão com um banco de dados//  
+            String serverName = "72.13.93.206";    //caminho do servidor do BD  
+            String mydatabase ="geovane1992";        //nome do banco de dados  
+            String url = "jdbc:mysql://" + serverName + "/" + mydatabase;  
+            String username = "geovane1992";        //nome de um usuário do BD        
+            String password = "Geo1992*";      // senha de acesso 
+            connection = (Connection) DriverManager.getConnection(url, username, password); 
+            comando = (Statement) connection.createStatement(); 
+            return connection;  
+        }  catch (ClassNotFoundException e) {  //Driver não encontrado  
+            JOptionPane.showMessageDialog(null,"O driver expecificado nao foi encontrado!");
+            return null;  
+        } catch (SQLException e) {  
+            //Não conseguindo se conectar ao banco 
+            JOptionPane.showMessageDialog(null,"Nao foi possivel conectar ao Banco de Dados!");
+            return null;  
+        }  
+   
+   
+    
+}
+
+public void fechar() {  
+      try {  
+         comando.close();  
+         connection.close();  
+         System.out.println("Conexão Fechada");  
+      } catch (SQLException e) {  
+         System.out.println("Erro ao fechar conexão");  
+      }  
+   }  
+
+  
+}
+
--- CalendarioTarefas/src/calendariotarefas/CalendarioTarefas.java
+++ CalendarioTarefas/src/calendariotarefas/CalendarioTarefas.java
@@ -5,6 +5,8 @@
  */
 package calendariotarefas;
 
+import Conexao.Conexao;
+
 /**
  *
  * @author GAOliveira
@@ -15,7 +17,9 @@ public class CalendarioTarefas {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Conexao cn = new Conexao();
+        System.out.println(cn.getConexao());
+
     }
     
 }
