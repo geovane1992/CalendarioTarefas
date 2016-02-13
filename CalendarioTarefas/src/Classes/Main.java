edd5e57f2e75dffbbecd7fe1fdd0f915c544a768
GAOliveira <GAOliveira@GSA07.gsa.go>
2016-02-13 16:18:44 -0200

    Correção no while do menu principal  do cadastro de disciplina.

--- CalendarioTarefas/src/Classes/Main.java
+++ CalendarioTarefas/src/Classes/Main.java
@@ -51,73 +51,86 @@ public class Main {
        
        while(opcaoMenu != 0){ //Repetir menu
            opcaoMenu    = menuPrincipal();
-           
-           while(opcaoSubmenu != 0){ //Repetir submenu
-               if(opcaoMenu != 0){
-                    opcaoSubmenu = submenu();
+           if(opcaoMenu >= 0 && opcaoMenu <= 6){
+
+                while(opcaoSubmenu != 0){ //Repetir submenu
+                    if(opcaoMenu != 0){
+                         opcaoSubmenu = submenu();
+                     }
+
+                     switch(opcaoMenu){
+                         case 1: //Curso
+                         break;
+
+                         case 2: //Aluno
+                         break;
+
+                         case 3: // Disciplina
+                             Scanner cod = new Scanner(System.in);
+                             Scanner nome = new Scanner(System.in);
+                             int codigoDisc;
+                             String nomeDisc;
+
+                             switch(opcaoSubmenu){
+                                 case 1: //insere
+                                     System.out.println("Infome o código da Discipliana!");
+                                     codigoDisc = cod.nextInt();
+                                     System.out.println("Infome o nome da Discipliana!");
+                                     nomeDisc = nome.nextLine();
+
+                                     disciplina.setCodDisciplina(codigoDisc);
+                                     disciplina.setNome(nomeDisc);
+                                     daoDisciplina.insere(disciplina);
+
+                                 break;
+
+                                 case 2: //Edita
+                                     System.out.println("Digite o código da disciplina a ser modificada!");
+                                     codigoDisc = cod.nextInt();
+                                     System.out.println("Informe o novo nome para a disciplina!");
+                                     nomeDisc = nome.nextLine();
+                                     disciplina.setCodDisciplina(codigoDisc);
+                                     disciplina.setNome(nomeDisc);
+                                     daoDisciplina.atualizar(disciplina);
+                                 break;
+
+                                 case 3: //Exclui
+                                     System.out.println("Digite o código da disciplina a ser eliminada!");
+                                     codigoDisc = cod.nextInt();
+                                     daoDisciplina.apagar(codigoDisc);
+                                 break;
+
+                                 case 4: // Lista
+                                     Vector<Disciplina> dis = daoDisciplina.buscarTodos();
+                                     for(int i = 0; i < dis.size(); i ++){
+                                          System.out.println(dis.get(i).getCodDisciplina() + " - " + dis.get(i).getNome());
+                                     }
+                                 break;
+
+                                 default:
+                                     System.out.println("Informe uma opção válida!");
+                                 break;
+                             }
+                         break;
+
+                         case 4: // Atividade
+                         break;
+
+                         case 5: // Turma
+                         break;
+
+                         case 6: // Importar Arquivo
+                         break;
+
+                         default:
+                             System.out.println("Informe uma opção válida!");
+                         break;
+
+                     }
                 }
-           
-                switch(opcaoMenu){
-                    case 1: //Curso
-                    break;
-
-                    case 2: //Aluno
-                    break;
-
-                    case 3: // Disciplina
-                        Scanner cod = new Scanner(System.in);
-                        Scanner nome = new Scanner(System.in);
-                        int codigoDisc;
-                        String nomeDisc;
-                        
-                        switch(opcaoSubmenu){
-                            case 1: //insere
-                                System.out.println("Infome o código da Discipliana!");
-                                codigoDisc = cod.nextInt();
-                                System.out.println("Infome o nome da Discipliana!");
-                                nomeDisc = nome.nextLine();
-                                
-                                disciplina.setCodDisciplina(codigoDisc);
-                                disciplina.setNome(nomeDisc);
-                                daoDisciplina.insere(disciplina);
-                                
-                            break;
-
-                            case 2: //Edita
-                                System.out.println("Digite o código da disciplina a ser modificada!");
-                                codigoDisc = cod.nextInt();
-                                System.out.println("Informe o novo nome para a disciplina!");
-                                nomeDisc = nome.nextLine();
-                                disciplina.setCodDisciplina(codigoDisc);
-                                disciplina.setNome(nomeDisc);
-                                daoDisciplina.atualizar(disciplina);
-                            break;
-
-                            case 3: //Exclui
-                                System.out.println("Digite o código da disciplina a ser eliminada!");
-                                codigoDisc = cod.nextInt();
-                                daoDisciplina.apagar(codigoDisc);
-                            break;
-
-                            case 4: // Lista
-                                Vector<Disciplina> dis = daoDisciplina.buscarTodos();
-                                for(int i = 0; i < dis.size(); i ++){
-                                     System.out.println(dis.get(i).getCodDisciplina() + " - " + dis.get(i).getNome());
-                                }
-                            break;
-                        }
-                    break;
-
-                    case 4: // Atividade
-                    break;
-
-                    case 5: // Turma
-                    break;
-
-                    case 6: // Importar Arquivo
-                    break;
-
-                }
+           }
+           else{
+               System.out.println("Informe uma opção válida!");
            }
            
        }
