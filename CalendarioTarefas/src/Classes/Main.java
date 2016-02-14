da43a89d0eaea961e8cab41eeff98e00362a5c2a
Eric Kriger <erickriger@gmail.com>
2016-02-14 00:26:43 -0200

    Implementação do cadastro de aluno

--- CalendarioTarefas/src/Classes/Main.java
+++ CalendarioTarefas/src/Classes/Main.java
@@ -72,18 +72,52 @@ public class Main {
                              
                              switch(opcaoSubmenu) {
                                  case 1: //insere
-                                     System.out.print("Informe a matricula do aluno: ");
+                                     System.out.print("Digite a matricula do aluno: ");
                                      matriculaAluno = Integer.parseInt(scanner.nextLine());
-                                     System.out.print("Informe o nome do aluno: ");
+                                     System.out.print("Digite o nome do aluno: ");
                                      nomeAluno = scanner.nextLine();
-                                     System.out.print("Informe o email do aluno: ");
+                                     System.out.print("Digite o email do aluno: ");
                                      emailAluno = scanner.nextLine();
                                      
-                                     Aluno aluno = new Aluno(matriculaAluno, nomeAluno, emailAluno);
-                                     daoAluno.insere(aluno);
+                                     Aluno novoAluno = new Aluno(matriculaAluno, nomeAluno, emailAluno);
+                                     daoAluno.insere(novoAluno);
                                  break;
                                      
+                                 case 2: // atualiza
+                                     System.out.print("Digite a matricula do aluno a ser modificado: ");
+                                     matriculaAluno = Integer.parseInt(scanner.nextLine());
+                                     System.out.println("Digite o novo nome do aluno: ");
+                                     nomeAluno = scanner.nextLine();
+                                     System.out.println("Digite o novo email do aluno: ");
+                                     emailAluno = scanner.nextLine();
                                      
+                                     Aluno alunoAtualizado = new Aluno(matriculaAluno, nomeAluno, emailAluno);
+                                     daoAluno.atualizar(alunoAtualizado);
+                                 break;
+                                     
+                                 case 3: // exclui
+                                     System.out.print("Digite a matricula do aluno a ser excluido: ");
+                                     matriculaAluno = Integer.parseInt(scanner.nextLine());
+                                     daoAluno.apagar(matriculaAluno);
+                                 break;
+                                     
+                                 case 4: // lista
+                                     Vector<Aluno> alunos = daoAluno.buscarTodos();
+                                     
+                                     for(int i = 0; i < alunos.size(); i ++){
+                                         StringBuilder stringAluno = new StringBuilder();
+                                         stringAluno.append(alunos.get(i).getMatricula());
+                                         stringAluno.append(" - ");
+                                         stringAluno.append(alunos.get(i).getNome());
+                                         stringAluno.append(" - ");
+                                         stringAluno.append(alunos.get(i).getEmail());
+                                         System.out.println(stringAluno.toString());
+                                     }
+                                 break;
+                                    
+                                 default:
+                                     System.out.println("Opcao invalida!");
+                                 break;
                              }
                              
                          break;
